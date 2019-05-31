ca715638b17ccb510f747b182111265338897297
C3real-kill3r <brybzi@gmail.com>
2019-05-31 11:55:05 +0300

    finish implementation of comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_100427) do
+ActiveRecord::Schema.define(version: 2019_05_30_172031) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
